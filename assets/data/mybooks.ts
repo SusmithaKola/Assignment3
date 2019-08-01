1a46e85cd137d643817b9dd73f74f99702586e3b
SusmithaKola <52940883+SusmithaKola@users.noreply.github.com>
2019-08-01 07:57:14 -0400

    Add files via upload

---
+++ assets/data/mybooks.ts
@@ -0,0 +1,36 @@
+import { Book } from "../../app/bookInfo";
+
+
+
+
+export const MyBooks: Book[] = [
+    {
+        name: 'The Friend Zone',
+        author: 'Abby Jimenez',
+        
+        genre: 'Romance',
+        publishedYear: '2019',
+        picture: 'assets/images/friend zone.jpg'
+    },
+    {
+        name: 'The Chasing Red',
+        author: 'Isabelle Ronin',
+        genre: 'Fiction',
+        publishedYear: '2017',
+        picture: 'assets/images/chasing red.jpg'
+    },
+    {
+        name: 'The Secret',
+        author: 'Rhonda Byrne',
+        genre: 'Self-help Book',
+        publishedYear: '2006',
+        picture: 'assets/images/secret.jpg'
+    },
+    {
+        name: 'Half Girlfriend',
+        author: 'Chetan Bhagat',
+        genre: 'Romance novel',
+        publishedYear: '2014',
+        picture: 'assets/images/half girlfriend.jpg'
+    }
+];
